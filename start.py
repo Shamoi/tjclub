d859594e090b12a55e07f3345f6dd73f535d1c16
Seva Zhidkov <zhidkovseva@gmail.com>
2015-05-06 14:03:54 +0400

    Исправлена IndexError после получения сообщения

--- start.py
+++ start.py
@@ -1,35 +1,54 @@
 import requests
 import json
 import time
+import re
 from send_yo import send
 from config import ACCESS_KEY, COMMUNITY_ID
 
-vk_api_url = 'https://api.vk.com/method/wall.get?owner_id={owner_id}&count=1'.format(
-    owner_id=str(COMMUNITY_ID)
+# Регулярное выражения для поиска URL
+url_regexp = re.compile(r'[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)',
+                        re.IGNORECASE)
+vk_api_url = 'https://api.vk.com/method/wall.get?owner_id={owner_id}&count={count}'.format(
+    owner_id=str(COMMUNITY_ID),
+    count=1
 )
+response = json.loads(requests.get(vk_api_url).text)
 
+# Если есть закрепленная запись, то мы в дальнейшем берем сразу
+# две записи и работаем со второй. Иначе - берем одну и
+# работаем с ней.
+if 'is_pinned' in response['response'][1]:
+    n = 2
+else:
+    n = 1
+
+vk_api_url = 'https://api.vk.com/method/wall.get?owner_id={owner_id}&count={count}'.format(
+    owner_id=str(COMMUNITY_ID),
+    count=n
+)
 try:
     last_post_id = int(open('last_post_id.txt').read())
 except FileNotFoundError:
-    response = json.loads(requests.get(vk_api_url).text)
-    last_post_id = response['response'][1]['id']
+    last_post_id = response['response'][n]['id']
 
 
 while True:
     response = json.loads(requests.get(vk_api_url).text)
-    new_post_id = response['response'][1]['id']
+    try:
+        new_post_id = response['response'][n]['id']
+    except IndexError:
+        time.sleep(5)
+        continue
     if new_post_id != last_post_id:
         last_post_id = new_post_id
 
-        # Ищем последнюю строчку в посте, которая является
-        # ссылкой на полный материал (в "новости часа").
-        # Если этой ссылки нет, игнорируем запись.
-        try:
-            post_link = response['response'][1]['text'].split('<br>')[-1]
-        except KeyError:
-            continue
-
-        send(key=ACCESS_KEY, link=post_link)
+        # Ищем URL в нужном посте и отправляем его
+        # как ссылку в Yo
+        post_link = re.search(url_regexp, response['response'][n]['text'])
+        if post_link is not None:
+            send_url = post_link.string[post_link.start():post_link.end()]
+            send(key=ACCESS_KEY,
+                 link=send_url)
 
     open('last_post_id.txt', 'w').write(str(last_post_id))
     time.sleep(5)
